8b0b54ded99d4f7b4b52f594ec46a429f43e7019
Franck YVONNET <franck@yvonnet.fr>
2024-12-10 15:21:18 +0100

    use tree to store catalog of frequencies

--- day08-part2.asm
+++ day08-part2.asm
@@ -3,17 +3,39 @@
 
 	.globl	_start
 
+	.set	COUNT, 		-4
+	.set	COORD_X, 	 0
+	.set	COORD_Y, 	 4
+	#.set	FREQ, 		 8
+	.set	FLAG, 		 8
+
+	.set	DATA_SET, 	 0
+	.set	DATA_LIMIT, 	 8
+
+	.set	ENTRY_LST, 	 0
+	.set	ENTRY_COUNT, 	 8
+	.set	ENTRY_FREQ, 	12
+	.set	ENTRY_SIZE, 	16
+
+	.set	NODE_COORDS, 	 0
+	.set	NODE_COORD_X, 	 0
+	.set	NODE_COORD_Y, 	 4
+	.set	NODE_NEXT, 	 8
+	.set	NODE_SIZE, 	16
+
 
 	.section .rodata
 filename:
 	.string	"inputs/day08"
 ansfmt:	.string	"Part %d: %d\n"
+entryfmt: .string "freq=%c, count=%d, coords="
+coordfmt: .string " (%d,%d)"
 
 
 
 	.bss
 	.balign 8
-	.set	ARENA_SIZE, 64*1024
+	.set	ARENA_SIZE, 1024*1024
 arena:	.space	ARENA_SIZE
 
 
@@ -23,84 +45,80 @@ arena:	.space	ARENA_SIZE
 
 
 	create_alloc_func alloc, arena, arena
+	create_free_func free, arena, arena
 
 
 	func_begin _start
 _start:
 	la	a0, filename
-	call	map_input_file
+	call	open_input_file
 	mv	s10, a0
-	add	s11, a0, a1
-
-	sub	sp, sp, a1
-	li	t0, 0b1111
-	not	t0, t0
-	and	sp, sp, t0
-	add	s0, sp, 4
 
 	la	a0, arena
 	li	a1, ARENA_SIZE
 	call	arena_init
 
+	# set of antinode coordinates
 	la	a0, compar_coords
 	la	a1, alloc
 	clr	a2
 	call	redblacktree_init
 	mv	s3, a0
 
+	# frequencies catalog
+	la	a0, compar_entries
+	la	a1, alloc
+	la	a2, free
+	call	redblacktree_init
+	mv	s4, a0
 
-	# copy map to memory
+	# line buffer
+	la	a0, 64
+	call	alloc
+	mv	s2, a0
+
 	mv	a0, s10
-	call	line_length
-	sw	a0, -4(s0)
-	li	t0, '\n'
-	mv	t2, s0
-loop_read_map:
-	lb	t1, (s10)
-	inc	s10
-	beq	t0, t1, skip_nl
-	sb	t1, (t2)
-	inc	t2
-skip_nl:
-	bne	s10, s11, loop_read_map
-	sb	zero, (t2)
+	mv	a1, s2
+	call	read_input_line
+	mv	s0, a0
 
-	# antennas list terminator
+	li	s11, '.'
+	clr	s9			# row number
+loop_rows:
+	clr	s8			# column number
+	mv	s7, s2
+loop_chars:
+	lb	t0, (s7)
+	beqz	t0, loop_chars_end
+	beq	t0, s11, skip_char
+	mv	a0, s4
+	mv	a1, s8
+	mv	a2, s9
+	mv	a3, t0
+	call	catalog_insert
+skip_char:
+	inc	s7
+	inc	s8
+	j	loop_chars
+loop_chars_end:
+	inc	s9
+	mv	a0, s10
+	mv	a1, s2
+	call	read_input_line
+	bgtz	a0, loop_rows
+	
 	dec	sp, 16
-	sb	x0, 8(sp)
+	sd	s3, DATA_SET(sp)
+	sd	s0, DATA_LIMIT(sp)
 
-	# load antennas
-	mv	t0, s0
-	clr	t1
-	li	t2, '.'
-	lw	t3, -4(s0)
-	clr	s1
-loop_get_antennas:
-	lb	t4, (t0)
-	beqz	t4, loop_get_antennas_end
-	beq	t4, t2, skip_ant
-	inc	s1
-	dec	sp, 16
-	rem	t5, t1, t3
-	sw	t5, 0(sp)
-	div	t5, t1, t3
-	sw	t5, 4(sp)
-	sb	t4, 8(sp)
-skip_ant:
-	inc	t1
-	inc	t0
-	j	loop_get_antennas
-loop_get_antennas_end:
-	mv	s2, sp
-	mv	s4, sp
-
-	mv	a0, s2
-	mv	a1, s1
-	li	a2, 16
-	la	a3, compar_antennas
-	call	quicksort
-
-
+	mv	a0, s4
+	#la	a1, print_entry
+	la	a1, process_entry
+	mv	a2, sp
+	call	redblacktree_inorder
+	
+	
+	
 loop:
 	mv	a0, s0
 	mv	a1, s3
@@ -110,33 +128,6 @@ loop:
 	lb	t0, 8(s2)
 	bnez	t0, loop
 
-
-	# add antennas from frequencies with more than one antenna
-
-loop_add_antennas:
-	lb	s5, 8(s4)
-	beqz	s5, loop_add_antennas_end
-	add	t2, s4, 16
-	lb	t1, 8(t2)
-	bne	s5, t1, loop_add_antennas_next		# skip frequency with only 1 antenna
-
-loop_add_frequency:
-	mv	a0, s0
-	mv	a1, s3
-	lw	a2, 0(s4)
-	lw	a3, 4(s4)
-	call	set_insert
-	inc	s4, 16
-	lb	t0, 8(s4)
-	beq	t0, s5, loop_add_frequency
-
-	j	loop_add_antennas
-
-loop_add_antennas_next:
-	mv	s4, t2
-	j	loop_add_antennas
-
-loop_add_antennas_end:
 
 	mv	a0, s3
 	call	redblacktree_count_nodes
@@ -149,8 +140,114 @@ loop_add_antennas_end:
 	func_end _start
 
 
+	# a0: entry
+	# a1: data
+	func_begin process_entry
+process_entry:
+	dec	sp, 64
+	sd	ra,  0(sp)
+	sd	s0,  8(sp)
+	sd	s1, 16(sp)
 
-	# a0: map
+	mv	s0, a0
+	mv	s1, a1
+
+	lw	t1, ENTRY_COUNT(s0)
+	li	t0, 1
+	beq	t1, t0, process_entry_end
+	dec	sp, 16
+	sb	x0, FLAG(sp)
+	ld	t0, ENTRY_LST(s0)
+	li	t1, 1
+	ld	s0, DATA_LIMIT(s1)
+	ld	s1, DATA_SET(s1)
+loop_copy_coords:
+	dec	sp, 16
+	ld	t2, NODE_COORDS(t0)
+	sd	t2, 0(sp)
+	sb	t1, 8(sp)
+	ld	t0, NODE_NEXT(t0)
+	bnez	t0, loop_copy_coords
+
+	mv	a0, s0
+	mv	a1, s1
+	mv	a2, sp
+	call	antinode_same_freq
+
+loop_add_antennas:
+	mv	a0, s0
+	mv	a1, s1
+	lw	a2, 0(sp)
+	lw	a3, 4(sp)
+	call	set_insert
+	inc	sp, 16
+	lb	t0, 8(sp)
+	bnez	t0, loop_add_antennas
+	inc	sp, 16
+process_entry_end:
+	ld	ra,  0(sp)
+	ld	s0,  8(sp)
+	ld	s1, 16(sp)
+	inc	sp, 64
+	ret
+	func_end process_entry
+
+
+	# a0: catalog
+	# a1: x
+	# a2: y
+	# a3: freq
+	func_begin catalog_insert
+catalog_insert:
+	dec	sp, 48
+	sd	ra,  0(sp)
+	sd	s0,  8(sp)
+	sd	s1, 16(sp)
+	sd	s2, 24(sp)
+	sd	s3, 32(sp)
+
+	mv	s0, a0
+	mv	s1, a1
+	mv	s2, a2
+	mv	s3, a3
+
+	la	a0, ENTRY_SIZE
+	call	alloc
+	sb	a3, ENTRY_FREQ(a0)
+	sd	x0, ENTRY_LST(a0)
+	sw	x0, ENTRY_COUNT(a0)
+
+	mv	a1, a0
+	mv	a0, s0
+	call	redblacktree_insert_or_free
+	mv	s0, a0
+
+	lw	t0, ENTRY_COUNT(s0)
+	inc	t0
+	sw	t0, ENTRY_COUNT(s0)
+
+	la	a0, NODE_SIZE
+	call	alloc
+
+	sw	s1, NODE_COORD_X(a0)
+	sw	s2, NODE_COORD_Y(a0)
+
+	ld	t0, ENTRY_LST(s0)
+	sd	t0, NODE_NEXT(a0)
+	sd	a0, ENTRY_LST(s0)
+	
+	ld	ra,  0(sp)
+	ld	s0,  8(sp)
+	ld	s1, 16(sp)
+	ld	s2, 24(sp)
+	ld	s3, 32(sp)
+	inc	sp, 48
+	ret
+	func_end catalog_insert
+
+
+
+	# a0: coordinates limit
 	# a1: set
 	# a2: antennas
 	func_begin antinode_same_freq
@@ -189,7 +286,7 @@ antinode_same_freq_end:
 	func_end antinode_same_freq
 
 
-	# a0: map
+	# a0: coordinates limit
 	# a1: set
 	# a2: current antenna
 	# a3: following antennas
@@ -232,7 +329,7 @@ loop_antinode_followings_end:
 
 
 
-	# a0: map
+	# a0: coordinates limit
 	# a1: set
 	# a2: antenna x
 	# a3: antenna y
@@ -246,11 +343,8 @@ set_insert:
 
 	bltz	a2, set_insert_ret
 	bltz	a3, set_insert_ret
-
-	lw	t2, -4(a0)
-
-	bge	a2, t2, set_insert_ret
-	bge	a3, t2, set_insert_ret
+	bge	a2, a0, set_insert_ret
+	bge	a3, a0, set_insert_ret
 
 	mv	a0, a1
 	slli	a1, a2, 32
@@ -372,6 +466,16 @@ antinode:
 
 
 	
+	func_begin compar_entries
+compar_entries:	
+	lb	t0, ENTRY_FREQ(a0)
+	lb	t1, ENTRY_FREQ(a1)
+	sub	a0, t0, t1
+	ret
+	func_end compar_entries
+
+
+
 	func_begin compar_antennas
 compar_antennas:	
 	lb	t0, 8(a0)
@@ -388,5 +492,37 @@ compar_coords:
 	ret
 	func_end compar_coords
 
+
+
+print_entry:
+	dec	sp, 32
+	sd	ra, (sp)
+	sd	s0, 8(sp)
+	sd	s1, 16(sp)
+
+	mv	s0, a0
+	ld	s1, ENTRY_LST(s0)
+	la	a0, entryfmt
+	lb	a1, ENTRY_FREQ(s0)
+	lw	a2, ENTRY_COUNT(s0)
+	call	printf
+
+	
+loop_print_coords:
+	la	a0, coordfmt
+	lw	a1, NODE_COORD_X(s1)
+	lw	a2, NODE_COORD_Y(s1)
+	call	printf
+	ld	s1, NODE_NEXT(s1)
+	bnez	s1, loop_print_coords
+	la	a0, '\n'
+	call	putc
+
+
+	ld	ra, (sp)
+	ld	s0, 8(sp)
+	ld	s1, 16(sp)
+	inc	sp, 32
+	ret
 
 
