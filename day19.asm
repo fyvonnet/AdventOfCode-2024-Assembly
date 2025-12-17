d204012fda54c0ff7a715a8e8d5467d966d7f972
Franck YVONNET <franck@yvonnet.fr>
2025-12-17 10:27:14 +0100

    rewrite to use trie

--- day19.asm
+++ day19.asm
@@ -1,242 +1,218 @@
 	.include "macros.inc"
 	.include "memory.inc"
 
-	.globl	_start
-
 
 
 	.section .rodata
 filename:
 	.string	"inputs/day19"
 ansfmt:	.string	"Part %d answer: %d\n"
+colors: .string  "wubrg"
+
+        .set    CHUNKS_COUNT,   4096
+        .set    CHUNKS_SIZE,    64
+        .set    POOL_SIZE,      8 + (CHUNKS_COUNT * CHUNKS_SIZE)
 
 
 
 	.bss
 	.balign 8
-        .set    CACHE_LEN, 64
-cache:  .space  8 * CACHE_LEN
-cache_end:
-        .size   cache, . - cache
+pool:   .space  POOL_SIZE
+        .size   pool, POOL_SIZE
+ranks:  .space  128
+        .size   ranks, 128
+cache:  .space  64*8
+        .size   cache, 64*8
 
 
 
 	.text
 	.balign 8
 
+
+
+	.globl	_start
 	func_begin _start
 _start:
+        la      a0, pool
+        li      a1, CHUNKS_COUNT
+        li      a2, CHUNKS_SIZE
+        call    pool_init
+
+        la      a0, colors
+        la      a1, ranks
+        la      a2, pool
+        call    trie_init
+        mv      s0, a0
+        ld      s9, (a0)
 
 	la	a0, filename
 	call	map_input_file
 	mv	s10, a0
 	add	s11, a0, a1
 
-	# allocate space for strings
-	sub	sp, sp, a1
-	li	t0, 0b1111
-	not	t0, t0
-	and	sp, sp, t0
-	mv	s9, sp
-
-	# allocate space for 512 pattern pointers
-	li	t0, 8*512
-	sub	sp, sp, t0
-	mv	s0, sp
-	
-	# allocate space for 512 design pointers
-	sub	sp, sp, t0
-	mv	s1, sp
-	
-	clr	s6
-	li	s7, '\n'
-	mv	s8, s0
-loop_parse_patterns:
-	inc	s6
-	mv	a0, s10
-	mv	a1, s9
-	call	parse_string
-	sd	s9, (s8)
-	inc	s8, 8
-	mv	s10, a0
-	mv	s9, a1
-	lb	t0, (s10)
-	beq	t0, s7, loop_parse_patterns_end
-	inc	s10, 2
-	j	loop_parse_patterns
-loop_parse_patterns_end:
-	sd	zero, (s8)
-	nop
-
-	inc	s10, 2
-
-	mv	s8, s1
-loop_parse_designs:
-	mv	a0, s10
-	mv	a1, s9
-	call	parse_string
-	sd	s9, (s8)
-	inc	s8, 8
-	addi	s10, a0, 1
-	mv	s9, a1
-	blt	s10, s11, loop_parse_designs
-	sd	zero, (s8)
-
-	mv	s10, s0
-	mv	s11, s6
-
-	mv	a0, s10
-	mv	a1, s11
-	li	a2, 8
-	la	a3, compar_strings
-	call	quicksort
-
-	clr	s2
-	clr	s3
+loop_insert_patterns:
+        mv      a0, s0
+        mv      a1, s10
+        call    trie_insert
+        add     s10, a0, 2
+        lb      t0, (a0)
+        li      t1, ','
+        beq     t0, t1, loop_insert_patterns
+        
+        clr     s1
+        clr     s2
 loop:
-        la      t0, cache
-        li      t1, 64
-        li      t2, -1
-loop_init_cache:
-        sd      t2, (t0)
-        inc     t0, 8
-        dec     t1
-        bnez    t1, loop_init_cache
+        mv      a0, s10
+        call    count_ways
+        beqz    a0, impossible
+        inc     s1
+        add     s2, s2, a0
+impossible:
 
-	ld	a0, (s1)
-	inc	s1, 8
-	beqz	a0, loop_end
-        clr     a1
-	call	count_ways
-        li      a1, 0
-	beqz	a0, loop
-	inc	s2
-	add	s3, s3, a0
-	j	loop
-loop_end:	
-	
-	la	a0, ansfmt
-	li	a1, 1
-	mv	a2, s2
-	call	printf
-	
-	la	a0, ansfmt
-	li	a1, 2
-	mv	a2, s3
-	call	printf
+        # skip to next input line
+        li      t1, '\n'
+loop_skip:
+        lb      t0, (s10)
+        beq     t0, t1, loop_skip_end
+        inc     s10
+        j       loop_skip
+loop_skip_end:
+        inc     s10
+        blt     s10, s11, loop
+
+        la      a0, ansfmt
+        li      a1, 1
+        mv      a2, s1
+        call    printf
+
+        la      a0, ansfmt
+        li      a1, 2
+        mv      a2, s2
+        call    printf
 
 	exit
 	func_end _start
 
 
 
-	# a0: design
-	func_end count_ways
+        # a0: string
+        func_begin count_ways
 count_ways:
-	dec	sp, 96
-	sd	x0,  0(sp)
-	sd	x0,  8(sp)
-	sd	x0, 16(sp)
-	sd	sp, 24(sp)
-	sd	ra, 32(sp)
-	sd	s0, 40(sp)
-	sd	s1, 48(sp)
-	sd	s2, 56(sp)
-	sd	s3, 64(sp)
-	sd	s4, 80(sp)
+        dec     sp, 16
+        sd      ra,  0(sp)
 
-	mv	s0, a0
+        mv      t0, a0
+        la      t1, cache
+        li      t2, '\n'
+        li      t4, -1
+loop_init_cache:
+        sd      t4, (t1)
+        inc     t0
+        inc     t1, 8
+        lb      t3, (t0)
+        bne     t3, t2, loop_init_cache
+
+        # hardcode termination condition in cache:
+        # one way found if end of design reached
+        li      t4, 1
+        sd      t4, (t1)
+
+        la      a1, cache
+        call    count_ways_rec
+
+        ld      ra,  0(sp)
+        inc     sp, 16
+        ret
+        func_end count_ways
+
+
+
+        # a0: string
+        # a1: cache
+        func_begin count_ways_rec
+count_ways_rec:
+        dec     sp, 64
+        #sd      x0,  0(sp)
+        sd      ra,  8(sp)
+        sd      s0, 16(sp)
+        sd      s1, 24(sp)
+        sd      s2, 32(sp)
+        sd      s3, 40(sp)
+        sd      s4, 48(sp)
+
+        ld      s2, (a1)
+        bltz    s2, not_in_cache
+        j       count_ways_rec_ret
+not_in_cache:
+
+        mv      s0, a0
         mv      s1, a1
+        mv      s4, sp
 
-	lb	t0, (s0)
-	beqz	t0, count_ways_found
+        mv      a1, sp
+        call    count_matches
+        mv      s3, a0
 
-        la      t0, cache
-        slli    t1, s1, 3
-        add     s3, t0, t1
-        ld      a0, (s3)
-        bltz    a0, cache_fail
-        j	count_ways_ret
-
-cache_fail:
-	clr	s4
-	clr	s2
-loop_search_prefs:
-
-	add	t1, s0, s4
-	lb	t2, (t1)
-	beqz	t2, loop_search_prefs_end
-	add	t1, sp, s4
-	sb	t2, (t1)
-
-	mv	a0, s10
-	mv	a1, s11
-	li	a2, 8
-	la	a3, compar_strings
-	addi	a4, sp, 24
-	call	binsearch
-	beqz	a0, loop_search_prefs_next
-
-	addi	t0, s4, 1
-	add	a0, s0, t0
+        clr     s2
+loop_count_ways_rec:
+        beqz    s3, loop_count_ways_rec_end
+        lb      t0, (s4)
+        add     a0, s0, t0
+        mul8    t0
         add     a1, s1, t0
-	call	count_ways
-	add	s2, s2, a0
-
-loop_search_prefs_next:
-	inc	s4
-	li	t0, 8
-	blt	s4, t0, loop_search_prefs
-loop_search_prefs_end:
-	sd	s2, (s3)
-	mv	a0, s2
-	j	count_ways_ret
-count_ways_found:
-	li	a0, 1
-count_ways_ret:
-	ld	ra, 32(sp)
-	ld	s0, 40(sp)
-	ld	s1, 48(sp)
-	ld	s2, 56(sp)
-	ld	s3, 64(sp)
-	ld	s4, 80(sp)
-	inc	sp, 96
-	ret
-	func_end count_ways
-	
-
-
-	# a0: source
-	# a1: destination
-	func_begin parse_string
-parse_string:
-	li	t0, 'a'
-	li	t1, 'z'
-loop_parse_string:
-	lb	t2, (a0)
-	blt	t2, t0, loop_parse_string_end
-	bgt	t2, t1, loop_parse_string_end
-	inc	a0
-	sb	t2, (a1)
-	inc	a1
-	j	loop_parse_string
-loop_parse_string_end:
-	sb	zero, (a1)
-	inc	a1
-	ret
-	func_end parse_string
+        call    count_ways_rec
+        add     s2, s2, a0
+        dec     s3
+        inc     s4
+        j       loop_count_ways_rec
+loop_count_ways_rec_end:
+        sd      s2, (s1)
+count_ways_rec_ret:
+        mv      a0, s2
+        ld      ra,  8(sp)
+        ld      s0, 16(sp)
+        ld      s1, 24(sp)
+        ld      s2, 32(sp)
+        ld      s3, 40(sp)
+        ld      s4, 48(sp)
+        inc     sp, 64
+        ret
+        func_end count_ways_rec
 
 
 
-	func_begin compar_strings
-compar_strings:
-	dec	sp, 16
-	sd	ra,  0(sp)
-	ld	a0, (a0)
-	ld	a1, (a1)
-	call	strcmp
-	ld	ra,  0(sp)
-	inc	sp, 16
-	ret
-	func_end compar_strings
+        # a0: string
+        # a1: array pointer
+        func_begin count_matches
+count_matches:
+        clr     t0              # matches counter
+        clr     t1              # string index
+        la      a3, ranks
+        mv      t3, s9
+loop_count_matches:
+        add     t2, a0, t1      
+        lb      t2, (t2)        # load character
+        add     t2, t2, a3
+        lb      t2, (t2)        # load rank
+        bltz    t2, count_matches_end
+        inc     t2              # trie pointers start at 1
+        mul8    t2
+        add     t2, t2, t3
+        ld      t3, (t2)        # load next node pointer
+        beqz    t3, count_matches_end
+        ld      t2, (t3)        # load end-of-word flag
+        beqz    t2, not_eow
+        inc     t0              # increment counter
+        addi    t2, t1, 1
+        sb      t2, (a1)        # save match length
+        inc     a1
+not_eow:
+        inc     t1
+        j       loop_count_matches
+count_matches_end:
+        mv      a0, t0
+        ret
+        func_end count_matches
+
 
